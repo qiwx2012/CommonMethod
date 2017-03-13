cdbf8ef6e8128144afb57d101539a60252c550dc
qiwx <qiwx@jingzhengu.com>
2017-03-13 10:28:06 +0800

    分支修改测试

---
+++ app/src/main/java/test/com/commonmethod/TestGit.java
@@ -0,0 +1,18 @@
+package test.com.commonmethod;
+
+import android.util.Log;
+
+/**
+ * @author: qiwx
+ * email: qiwx@jingzhengu.com
+ * @time: 2017/3/13  10:26
+ * @desc: 测试git合并方法
+ */
+
+public class TestGit {
+
+    private void test() {
+        Log.i("dd", "");
+    }
+
+}
